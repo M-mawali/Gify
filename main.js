4371beb53be7104410c382a06efa525455b9ecb4
M-mawali <muhmadmawali@gmail.com>
2022-03-03 11:43:36 +0100

    created the api function to the giphy website

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("mama")
+function sendApiRequest(){
+    var userInput = document.getElementById("input").value
+    console.log(userInput)
+    
+    var giphyApiKey = "zvT1Y2w1i19IByqtSXJlUBc9t2lShQDR"
+    var giphyApiUrl = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}`
+    
+    fetch(giphyApiUrl).then(function(data){
+        return data.json()
+    })
+    .then (function(json){
+        console.log(json.data[0].images.fixed_height.url)
+        var imgPath= json.data[0].images.fixed_height.url
+        var img = document.createElement("img")
+        img.setAttribute("src", imgPath)
+        document.body.appendChild(img)
+    })
+}
