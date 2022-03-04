b08fd53397fa6cd538a5a74c1f26e01fdb1c9870
M-mawali <muhmadmawali@gmail.com>
2022-03-04 19:34:08 +0100

    reworked the search function to find multi results

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-function sendApiRequest(){
+
+// function geting data from the giphy website using the api and linking it with the search 
+function searchGify(){
     var userInput = document.getElementById("input").value
     console.log(userInput)
-    
+
     var giphyApiKey = "zvT1Y2w1i19IByqtSXJlUBc9t2lShQDR"
-    var giphyApiUrl = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}`
+    var giphyApiUrl = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&api_key=${giphyApiKey}&limit=15`
     
-    fetch(giphyApiUrl).then(function(data){
-        return data.json()
-    })
-    .then (function(json){
+    fetch(giphyApiUrl)
+    .then(function(res){
+        return res.json()
+    }).then(function(json){
         console.log(json.data[0].images.fixed_height.url)
-        var imgPath= json.data[0].images.fixed_height.url
+        json.data.forEach(function(obj){
+            console.log(obj.images.fixed_height.url)
+        var imgPath= obj.images.fixed_height.url
         var img = document.createElement("img")
         img.setAttribute("src", imgPath)
         document.body.appendChild(img)
     })
+})
 }
+
+
+// function to clean the results by new search 
+// how to show more than 1 result 
